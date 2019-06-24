b3c61d40892ef5d39e3203f225ebc9286663ae4e
bhovdair <bhovdair@sgp-dkp.com>
2019-06-24 20:14:22 +0700

    test

--- app/config/config.php
+++ app/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] .':'. $_SERVER['SERVER_PORT']. '/bp3tki';
-$config['base_url'] = 'http://bp3tkijakarta.com/';
+$config['base_url'] = 'http://' . $_SERVER['SERVER_NAME'] .':'. $_SERVER['SERVER_PORT']. '/bp3tkijakarta/';
+// $config['base_url'] = 'http://bp3tkijakarta.com/';
 
 /*
 |--------------------------------------------------------------------------
